7e02c42e184eff8f94c6e8999882dcdd05e34538
Ryans127 <155965539+Ryans127@users.noreply.github.com>
2024-04-15 14:35:37 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,7 +18,6 @@ public class Question4
      */
 
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter a number: ");
     int n = in.nextInt();
     for(int i = 0; i < n; i++)
       System.out.print("*");
